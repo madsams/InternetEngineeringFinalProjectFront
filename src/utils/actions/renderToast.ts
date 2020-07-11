4c949440d759962fe67b0d1cae655141aed8dc27
Mohammad Seyfayi <madsams024@gmail.com>
2020-07-11 17:46:18 +0430

    renderToast.ts added

---
+++ src/utils/actions/renderToast.ts
@@ -0,0 +1,19 @@
+import {toastr} from 'react-redux-toastr';
+
+const ToastrOptions = {
+    showCloseButton: false,
+    closeOnToastrClick: true,
+};
+
+export enum toastTypes {
+    LIGHT = 'light',
+    MESSAGE = 'message',
+    INFO = 'info',
+    SUCCESS = 'success',
+    WARNING = 'warning',
+    ERROR = 'error',
+}
+
+const renderToast = (title: string, message: string, type: toastTypes) =>
+    toastr[type](title, message, ToastrOptions);
+export default renderToast;
