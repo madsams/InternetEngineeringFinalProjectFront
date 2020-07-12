8233590cb64276ff27ef54b82c30319e47c31c4d
Mohammad Seyfayi <madsams024@gmail.com>
2020-07-12 19:37:56 +0430

    BackButton.tsx make rtl

--- src/component/utils/header/BackButton.tsx
+++ src/component/utils/header/BackButton.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import {IconButton} from '@material-ui/core';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 import {useHistory} from 'react-router';
 import {Strings} from '../../../utils/types';
 import ITooltip from '../ITooltip';
+import {useDirection} from '../../../utils/hooks';
 
 const tooltipTitle: Strings = {
     en: 'Back',
@@ -12,6 +14,7 @@ const tooltipTitle: Strings = {
 
 const BackButton = () => {
     const history = useHistory();
+    const isRTL = useDirection();
 
     return (
         <ITooltip title={tooltipTitle}>
@@ -20,7 +23,7 @@ const BackButton = () => {
                 color="inherit"
                 aria-label="menu"
                 onClick={history.goBack}>
-                <ArrowBackIcon />
+                {isRTL ? <ArrowForwardIcon /> : <ArrowBackIcon />}
             </IconButton>
         </ITooltip>
     );
