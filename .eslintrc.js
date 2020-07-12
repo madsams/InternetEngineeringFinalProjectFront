ffaa67730f5043378bb6687e6529beb1ecb2b322
Mohammad Seyfayi <madsams024@gmail.com>
2020-07-12 15:44:20 +0430

    no-restricted-imports from .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,15 @@
 module.exports = {
-    parser: "babel-eslint",
-    plugins: [
-        "react",
-        "react-hooks"
-    ],
-    extends: [
-        "eslint:recommended",
-        "plugin:react/recommended",
-    ],
+    parser: 'babel-eslint',
+    plugins: ['react', 'react-hooks'],
+    extends: ['eslint:recommended', 'plugin:react/recommended'],
     env: {
         browser: true,
         node: true,
-        es6: true
+        es6: true,
     },
     rules: {
-        "no-restricted-imports": [
-            "error",
-            {
-                "patterns": ["@material-ui/*/*/*", "!@material-ui/core/test-utils/*"]
-            }
-        ],
-        "react-hooks/rules-of-hooks": "error",
-        "react-hooks/exhaustive-deps": "warn"
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn',
     },
     overrides: [
         {
@@ -29,11 +17,11 @@ module.exports = {
             parser: '@typescript-eslint/parser',
             rules: {
                 'no-undef': 'off',
-                'no-unused-vars': 'off'
+                'no-unused-vars': 'off',
             },
             parserOptions: {
-                sourceType: "module"
-            }
-        }
-    ]
+                sourceType: 'module',
+            },
+        },
+    ],
 };
