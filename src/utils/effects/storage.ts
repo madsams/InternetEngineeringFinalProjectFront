47ab78cb9e5c4da858a7e5a53991fd53f74a5b43
Mohammad Seyfayi <madsams024@gmail.com>
2020-07-14 00:01:39 +0430

    save language in localStorage

--- src/utils/effects/storage.ts
+++ src/utils/effects/storage.ts
@@ -6,10 +6,7 @@ export const setStorage = (key: string, value: any) => {
     }
 };
 
-export const getStorage = <T>(
-    key: string,
-    initialState: T | null = null,
-): T | null => {
+export const getStorage = <T>(key: string, initialState: T): T => {
     try {
         const serializedState = localStorage.getItem(key);
         if (serializedState === null) {
