713be96a127809242ae905c9ba1bc036d6a5a38a
Mohammad Seyfayi <madsams024@gmail.com>
2020-07-15 00:45:54 +0430

    wip

--- src/component/field/actions.ts
+++ src/component/field/actions.ts
@@ -2,8 +2,8 @@ import {Form} from '../../utils/types';
 import API from '../../utils/API';
 import {createDataActions} from '../../utils/generics';
 
-export const getForms = createDataActions<Form>('FORMS', API.forms);
+export const getForms = createDataActions<Form>('GET_FORMS', API.forms);
 export const getFilledForms = createDataActions<Form>(
-    'FILLED_FORMS',
+    'GET_FILLED_FORMS',
     API.filledForms,
 );
