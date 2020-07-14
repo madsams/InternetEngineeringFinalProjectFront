ce4565208262b543e76e0e5bd506947e716e6e1c
Mohammad Seyfayi <madsams024@gmail.com>
2020-07-14 16:59:34 +0430

    react-app-env.d.ts set

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,12 @@
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            NODE_ENV: 'development' | 'production';
+            REACT_APP_HOST: string;
+            PUBLIC_URL: string;
+        }
+    }
+}
+
+export {};
 /// <reference types="react-scripts" />
