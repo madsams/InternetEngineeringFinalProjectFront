02389d0f1233df254b83cd3e403bc4289808cf6b
Mohammad Seyfayi <madsams024@gmail.com>
2020-07-15 00:59:41 +0430

    NotMatch.tsx

--- src/component/utils/NotMatch.tsx
+++ src/component/utils/NotMatch.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import {LangBaseJson} from '../../utils/types';
+import {StringsJson} from '../../utils/types';
 import ITypography from './ITypography';
 
-const strings: LangBaseJson = {
-    en: 'This page is not exist',
-    fa: 'صفحه مورد نظر شما یافت نشد',
+const strings: StringsJson = {
+    code: {
+        en: '404',
+        fa: '۴۰۴',
+    },
+    text: {
+        en: 'This page is not exist',
+        fa: 'صفحه مورد نظر شما یافت نشد',
+    },
 };
 const NotMatch = () => (
-    <div className="d-flex flex-1 align-items-center justify-content-center">
-        <ITypography text={strings} variant={'h3'} />
+    <div className="d-flex flex-column flex-1 align-items-center justify-content-center h-100">
+        <ITypography
+            text={strings.code}
+            variant="h1"
+            align="center"
+            className="m-3"
+        />
+        <ITypography text={strings.text} variant="h4" align="center"/>
     </div>
 );
 export default NotMatch;
