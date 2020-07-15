197b29aa02888d285b8fec90c6376d168ec84637
Mohammad Seyfayi <madsams024@gmail.com>
2020-07-15 13:16:43 +0430

    InputSelect.tsx added

--- src/component/fieldAgent/form/input/InputSelect.tsx
+++ src/component/fieldAgent/form/input/InputSelect.tsx
@@ -1,10 +1,44 @@
 import React from 'react';
 import {IInputProps} from './types';
 import {Option} from '../../../../utils/types';
+import {FormControl, InputLabel, MenuItem, Select} from '@material-ui/core';
 
 interface InputSelectProps extends IInputProps {
     options: Array<Option>;
 }
 
-const InputSelect = ({options}: InputSelectProps) => <></>;
+const InputSelect = ({
+    name,
+    title,
+    value,
+    required,
+    onChange,
+    onBlur,
+    options,
+}: InputSelectProps) => {
+    const handleChange = (event: React.ChangeEvent<{value: unknown}>) =>
+        onChange(event.target.value as string);
+
+    return (
+        <FormControl>
+            <InputLabel id={name + 'label'}>{title}</InputLabel>
+            <Select
+                required={required}
+                labelId={name + 'label'}
+                id={name}
+                value={value}
+                onBlur={onBlur}
+                onChange={handleChange}>
+                {options.map((option) => (
+                    <MenuItem
+                        key={option.value}
+                        value={option.value}
+                        button={false}>
+                        {option.label}
+                    </MenuItem>
+                ))}
+            </Select>
+        </FormControl>
+    );
+};
 export default InputSelect;
