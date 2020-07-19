4b25dc419a5811990ee9fb7aa20e402d50d9e7da
Mohammad Seyfayi <madsams024@gmail.com>
2020-07-19 12:35:44 +0430

    FIELD_ added to field tpyes

--- src/component/fieldAgent/actions.ts
+++ src/component/fieldAgent/actions.ts
@@ -14,11 +14,11 @@ import {
 } from '../../utils/generics';
 import {
     FormValues,
-    GET_ALL_FORMS,
-    GET_FORM_ANSWER_DETAIL,
-    GET_FORM_ANSWERS,
-    GET_FORM_DETAIL,
-    GET_POLYGON_OF_LOCATION,
+    FIELD_GET_ALL_FORMS,
+    FIELD_GET_FORM_ANSWER_DETAIL,
+    FIELD_GET_FORM_ANSWERS,
+    FIELD_GET_FORM_DETAIL,
+    FIELD_GET_POLYGON_OF_LOCATION,
     PolygonsOfLocation,
     SUBMIT_FORM,
 } from './types';
@@ -26,9 +26,9 @@ import {
 export const getPolygonOfLocation = createGetRequestWithParamsActions<
     PolygonsOfLocation[],
     Location
->(GET_POLYGON_OF_LOCATION, api.polygon);
+>(FIELD_GET_POLYGON_OF_LOCATION, api.polygon);
 export const resetPolygonOfLocation = (): IDataAction<PolygonsOfLocation> => ({
-    type: GET_POLYGON_OF_LOCATION + _SUCCESS,
+    type: FIELD_GET_POLYGON_OF_LOCATION + _SUCCESS,
     payload: [],
 });
 
@@ -38,20 +38,20 @@ export const submitForm = createPostRequestWithIdActions<{values: FormValues}>(
 );
 
 export const getForms = createGetRequestSimpleActions<Form>(
-    GET_ALL_FORMS,
+    FIELD_GET_ALL_FORMS,
     api.forms,
 );
 
 export const getFormAnswers = createGetRequestSimpleActions<FormAnswer>(
-    GET_FORM_ANSWERS,
+    FIELD_GET_FORM_ANSWERS,
     api.formAnswers,
 );
 
 export const getSelectedFormDetail = createGetRequestWithIdActions<Form>(
-    GET_FORM_DETAIL,
+    FIELD_GET_FORM_DETAIL,
     api.forms,
 );
 
 export const getSelectedFormAnswerDetail = createGetRequestWithIdActions<
     FormAnswer
->(GET_FORM_ANSWER_DETAIL, api.formAnswers);
+>(FIELD_GET_FORM_ANSWER_DETAIL, api.formAnswers);
--- src/component/fieldAgent/reducer.ts
+++ src/component/fieldAgent/reducer.ts
@@ -5,38 +5,38 @@ import {
 } from '../../utils/generics';
 import {combineReducers} from 'redux';
 import {
-    GET_ALL_FORMS,
-    GET_FORM_ANSWER_DETAIL,
-    GET_FORM_ANSWERS,
-    GET_FORM_DETAIL,
-    GET_POLYGON_OF_LOCATION,
+    FIELD_GET_ALL_FORMS,
+    FIELD_GET_FORM_ANSWER_DETAIL,
+    FIELD_GET_FORM_ANSWERS,
+    FIELD_GET_FORM_DETAIL,
+    FIELD_GET_POLYGON_OF_LOCATION,
     PolygonsOfLocation,
     SUBMIT_FORM,
 } from './types';
 
-const formsReducer = createDataRequestReducer<Form[]>(GET_ALL_FORMS, []);
+const formsReducer = createDataRequestReducer<Form[]>(FIELD_GET_ALL_FORMS, []);
 
 const formAnswersReducer = createDataRequestReducer<FormAnswer[]>(
-    GET_FORM_ANSWERS,
+    FIELD_GET_FORM_ANSWERS,
     [],
 );
 
 const submitFormReducer = createRequestReducer(SUBMIT_FORM);
 
 const polygonsOfLocationReducer = createDataRequestReducer<PolygonsOfLocation>(
-    GET_POLYGON_OF_LOCATION,
+    FIELD_GET_POLYGON_OF_LOCATION,
     [],
     true,
 );
 
 const formDetailReducer = createDataRequestReducer<Form | undefined>(
-    GET_FORM_DETAIL,
+    FIELD_GET_FORM_DETAIL,
     undefined,
     true,
 );
 const formAnswerDetailReducer = createDataRequestReducer<
     FormAnswer | undefined
->(GET_FORM_ANSWER_DETAIL, undefined, true);
+>(FIELD_GET_FORM_ANSWER_DETAIL, undefined, true);
 
 const fieldReducer = combineReducers({
     forms: formsReducer,
