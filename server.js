d34b59aa1cf3f7f1023dd1d2d2418b100f787a01
Mohammad Seyfayi <madsams024@gmail.com>
2020-07-19 12:52:18 +0430

    wip

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'input.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(port, () => {
