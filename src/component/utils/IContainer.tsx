528214be736e43641b7dfd89588e52aaa4a9b257
Mohammad Seyfayi <madsams024@gmail.com>
2020-07-21 12:28:37 +0430

    wip

--- src/component/utils/IContainer.tsx
+++ src/component/utils/IContainer.tsx
@@ -7,7 +7,7 @@ interface IContainerProps {
 }
 
 const IContainer = ({children, className}: IContainerProps) => (
-    <div className="flex-1 justify-content-center bg-light">
+    <div className="flex-1 justify-content-center bg-light mb-4">
         <Container maxWidth="md" className={'mt-4 ' + className}>
             {children}
         </Container>
