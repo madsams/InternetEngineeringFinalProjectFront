4995e26a9688d4e069767e10a52c5d3c6bc49f8f
Mohammad Seyfayi <madsams024@gmail.com>
2020-07-24 21:08:20 +0430

    Router was messed up

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -5,7 +5,7 @@ export type IdApi = (id: ID) => string;
 
 const formsList: SimpleApi = '/api/forms';
 const formDetail: IdApi = (id: ID) => `/api/forms/${id}`;
-const answersList: SimpleApi = '/api/form-answers';
+const answersList: SimpleApi = '/api/users/form-answers';
 const answerDetail: IdApi = (id: ID) => `/api/form-answers/${id}`;
 const answerOfForm: IdApi = (id: ID) => `/api/forms/${id}/form-answers`;
 const areaTestPoint: SimpleApi = '/api/areas/testpoint';
